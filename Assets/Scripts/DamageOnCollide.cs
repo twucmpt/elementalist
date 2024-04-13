93c885e6da8e5fea5aa6b8e801b6fbf9f6e199d9
Bit Flipz <114410587+BitFlipz@users.noreply.github.com>
2024-04-13 00:54:59 -0400

    Fix damage dealing and add cooldown

--- Assets/Scripts/DamageOnCollide.cs
+++ Assets/Scripts/DamageOnCollide.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class DamageOnCollide : MonoBehaviour
 {
     public int damage;
+    public float cooldown;
+    private float currentCooldown;
+
+    void Update() {
+        if (currentCooldown > 0) currentCooldown -= Time.deltaTime;
+    }
+
     void OnCollisionStay2D(Collision2D collision)
     {
-        collision.gameObject.SendMessage("DealDamage", damage);
+        if (collision.gameObject.CompareTag("Player") & currentCooldown <= 0) {
+            collision.gameObject.SendMessage("DealDamage", damage);
+            currentCooldown = cooldown;
+        }
     }
 }
