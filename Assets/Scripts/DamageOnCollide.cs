622efb06f5ab5d5ef7b9f72bc23bdb60bdbe9440
samdelaney <sam@thedelaneys.net>
2024-04-12 23:15:04 -0700

    goblin attack, damage & death animations

--- Assets/Scripts/DamageOnCollide.cs
+++ Assets/Scripts/DamageOnCollide.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DamageOnCollide : MonoBehaviour
 {
     public int damage;
     public float cooldown;
     private float currentCooldown;
+
+    public UnityEvent onAttack1;
+    public UnityEvent onAttack2;
 
     void Update() {
         if (currentCooldown > 0) currentCooldown -= Time.deltaTime;
@@ -17,6 +21,10 @@ public class DamageOnCollide : MonoBehaviour
         if (collision.gameObject.CompareTag("Player") & currentCooldown <= 0) {
             collision.gameObject.SendMessage("DealDamage", damage);
             currentCooldown = cooldown;
+            if(Random.Range(0, 10) < 2)
+                onAttack2.Invoke();
+            else
+                onAttack1.Invoke();
         }
     }
 }
