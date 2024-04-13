92ef1c32679b248ed891a2b5b906bba02cc744de
samdelaney <sam@thedelaneys.net>
2024-04-12 21:22:34 -0700

    wizard pc idle & walk animations

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,11 +4,16 @@ public class Movement : MonoBehaviour
 {
     public float moveSpeed;
     protected Vector2 moveVal;
+    public Animator animator;
 
     protected virtual void FixedUpdate() {
         if ((transform.localScale.x < 0 && moveVal.x > 0) || (transform.localScale.x > 0 && moveVal.x < 0)) {
             transform.localScale=new Vector3(-transform.localScale.x,transform.localScale.y,transform.localScale.z);
         }
         transform.Translate(new Vector3(moveVal.x, moveVal.y, 0) * moveSpeed * Time.fixedDeltaTime);
+        
+        if(animator != null) {
+            animator.SetBool("IsMoving", moveVal != Vector2.zero);
+        }
     }
 }
