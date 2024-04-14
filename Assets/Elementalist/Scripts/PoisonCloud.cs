5f155fbed5e3938a9785b6c24930afe3a0f0878a
Bit Flipz <114410587+BitFlipz@users.noreply.github.com>
2024-04-13 21:30:58 -0400

    Remove spammy debug log

--- Assets/Elementalist/Scripts/PoisonCloud.cs
+++ Assets/Elementalist/Scripts/PoisonCloud.cs
@@ -14,7 +14,6 @@ public class PoisonCloud : Projectile
     }
 
     void DoTriggerStuff(Collider2D collider) {
-        Debug.Log($"Hit {collider.gameObject.name}");
         if (collider.gameObject.CompareTag("Enemy")) {
             collider.gameObject.SendMessage("DealDamage", stats.GetStat(StatType.Damage));
         }
