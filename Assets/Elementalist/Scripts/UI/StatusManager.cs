6614753e88f40f832f86af4e6e304cf771ed0528
samdelaney <sam@thedelaneys.net>
2024-04-14 03:41:21 -0700

    add score to UI

--- Assets/Elementalist/Scripts/UI/StatusManager.cs
+++ Assets/Elementalist/Scripts/UI/StatusManager.cs
@@ -1,21 +1,23 @@
 using UnityEngine.UI;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class StatusManager : MonoBehaviour {
   public Image HealthBar;
   public Image ExpBar;
   public TextMeshProUGUI LevelText;
+  public TextMeshProUGUI ScoreText;
   public Health playerHealth;
   public PlayerLeveling playerLeveling;
 
   void Update() {
     HealthBar.fillAmount = playerHealth.health / playerHealth.maxHealth;
     ExpBar.fillAmount = playerLeveling.PercentToNextLevel();
+    ScoreText.text = "Score: " + playerLeveling.GetScore();
   }
 
   public void SetLevel() {
-    Debug.Log("Setting level text to " + playerLeveling.Level.ToString() + ".");
     LevelText.text = playerLeveling.Level.ToString();
   }
 }
