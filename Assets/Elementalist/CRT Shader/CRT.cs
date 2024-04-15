1b935b333a5f3954ee9e193064d2204e6235557d
Bit Flipz <114410587+BitFlipz@users.noreply.github.com>
2024-04-14 22:57:29 -0400

    Added shader from my prev game

---
+++ Assets/Elementalist/CRT Shader/CRT.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CRT : MonoBehaviour {
+    public Shader crtShader;
+
+    [Range(1.0f, 10.0f)]
+    public float curvature = 1.0f;
+
+    [Range(1.0f, 100.0f)]
+    public float vignetteWidth = 30.0f;
+
+    private Material crtMat;
+
+    void Start() {
+        crtMat ??= new Material(crtShader);
+        crtMat.hideFlags = HideFlags.HideAndDontSave;
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        crtMat.SetFloat("_Curvature", curvature);
+        crtMat.SetFloat("_VignetteWidth", vignetteWidth);
+        Graphics.Blit(source, destination, crtMat);
+    }
+}
